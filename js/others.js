0b652c1b4e499e9b2df1b107fb9b3be809195bf3
Islam Dudaev <islamwolf23@gmail.com>
2018-04-07 23:49:09 +0100

    Add IP Address detection API

--- js/others.js
+++ js/others.js
@@ -1,3 +1,4 @@
+// #1 API - Chuck Norris Jokes
 var result = document.querySelector("#api-result");
 var getJokeBtn = document.querySelector('#newJoke'); 
 
@@ -13,3 +14,27 @@ function getNewJoke(){
 		});
 }
 getJokeBtn.addEventListener("click", getNewJoke);
+
+// #2 API - IP address info
+
+var ip = document.querySelector("#ip");
+var city = document.querySelector("#city");
+var region = document.querySelector("#region");
+var country = document.querySelector("#country");
+var post = document.querySelector("#post");
+var lat_long = document.querySelector("#lat_long");
+var timeZone = document.querySelector("#timeZone");
+var org = document.querySelector("#org");
+
+fetch("https://ipapi.co/json")
+	.then(response => response.json())
+	.then(info => {
+		ip.innerHTML = info.ip;
+		city.innerHTML = info.city;
+		region.innerHTML = info.region;
+		country.innerHTML = info.country_name;
+		post.innerHTML = info.postal;
+        lat_long.innerHTML = info.latitude + " / " + info.longitude;
+        timeZone.innerHTML = info.timezone + " (" + info.utc_offset + ")";
+        org.innerHTML = info.org;
+	});
